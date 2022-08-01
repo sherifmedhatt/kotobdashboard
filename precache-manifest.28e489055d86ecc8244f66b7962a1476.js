28ad8ca4990ced94f8f677a7935954e37570bf80
Sherif Medhat <sherifmedhat@MacBook-Pro.local>
2022-08-01 18:30:14 +0200

    Updates

---
+++ precache-manifest.28e489055d86ecc8244f66b7962a1476.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf1f2c18438c1fe715a912b08731ec6",
+    "url": "/kotobdashboard/index.html"
+  },
+  {
+    "revision": "722f41121c1bf633583e",
+    "url": "/kotobdashboard/static/css/2.4f2cacb2.chunk.css"
+  },
+  {
+    "revision": "4b2f6b9cc323cde4dcad",
+    "url": "/kotobdashboard/static/css/main.adeea6b0.chunk.css"
+  },
+  {
+    "revision": "722f41121c1bf633583e",
+    "url": "/kotobdashboard/static/js/2.8499d5c0.chunk.js"
+  },
+  {
+    "revision": "b1762af17f317f2b17bd41576e843ac8",
+    "url": "/kotobdashboard/static/js/2.8499d5c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a408cfec6b00581c5a",
+    "url": "/kotobdashboard/static/js/3.1815a65e.chunk.js"
+  },
+  {
+    "revision": "6e31a0db202a37e424180fec8d0f2d9a",
+    "url": "/kotobdashboard/static/js/3.1815a65e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c2d3681c62abad173e",
+    "url": "/kotobdashboard/static/js/4.5854e762.chunk.js"
+  },
+  {
+    "revision": "73aea2f6b249805336f747a1f65ca2b9",
+    "url": "/kotobdashboard/static/js/4.5854e762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9701e9d5aa0ada6ec9c7",
+    "url": "/kotobdashboard/static/js/5.fc33da5b.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "/kotobdashboard/static/js/5.fc33da5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2f6b9cc323cde4dcad",
+    "url": "/kotobdashboard/static/js/main.9726b99c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/kotobdashboard/static/js/main.9726b99c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38b49ae166335a5a188",
+    "url": "/kotobdashboard/static/js/runtime-main.b8038797.js"
+  },
+  {
+    "revision": "2e7c58832a256d1c3e719662152a84c4",
+    "url": "/kotobdashboard/static/media/kotoblogo.2e7c5883.png"
+  }
+]);
